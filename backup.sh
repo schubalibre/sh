8a8bf1842e10b8e6e6e880efed339e411e217f06
schubalibre <robertodziuba@googlemail.com>
2015-05-06 20:50:53 +0200

    backup rechtschreibfehler

--- backup.sh
+++ backup.sh
@@ -1,47 +1,47 @@
-SURFIX=$1
+SUFFIX=$1
 # maxdepth wie viele Level
 # type -f nur Files
-# -name wildcart für Endung 
-FILES=$(find . -maxdepth 1 -type f -name "*.$SURFIX")
+# -name wildcart für Endung
+FILES=$(find . -maxdepth 1 -type f -name "*.$SUFFIX")
 DIRECTORY=backup
 
-echo "Herzlich Wiillkommen. Mit diesem Programm können sie eine Sicherheitskopie ihrer Daten erstellen!"
+echo "Herzlich Willkommen. Mit diesem Programm können sie eine Sicherheitskopie ihrer Daten erstellen!"
 
-if [  ${#SURFIX} -eq 0 ]
+if [  ${#SUFFIX} -eq 0 ]
 then
-	echo "Es muss ein Surfix angegeben werden! z.B. txt, jpg oder php"
-	exit >&2
+    echo "Es muss ein Suffix angegeben werden! z.B. txt, jpg oder php"
+    exit >&2
 fi
 
 if [ ${#FILES} -le 0 ]
 then
-	echo "Es gibt leider keine Dateien mit der Endung '$SURFIX' in diesem Ordner."
-	exit >&2
+    echo "Es gibt leider keine Dateien mit der Endung '$SUFFIX' in diesem Ordner."
+    exit >&2
 fi
 
-if [ ! -d "$DIRECTORY" ]; 
+if [ ! -d "$DIRECTORY" ];
 then
-	./frage.sh "Soll ein $DIRECTORY Ordner angelegt werden?" "y" "n"
-	ANTWORT=$?
-	if [ $ANTWORT = 0 ]
-	then
-		mkdir "$DIRECTORY"
-		echo "./$DIRECTORY kreiert."
-	else
-		exit >&2
-	fi
+    ./frage.sh "Soll ein $DIRECTORY Ordner angelegt werden?" "y" "n"
+    ANTWORT=$?
+    if [ $ANTWORT = 0 ]
+    then
+        mkdir "$DIRECTORY"
+        echo "./$DIRECTORY kreiert."
+    else
+        exit >&2
+    fi
 fi
 
-for FILE in $FILES 
+for FILE in $FILES
 do
-	./frage.sh "Soll $FILE gesichert werden?" "y" "n"
-	ANTWORT=$?
-	if [ $ANTWORT = 0  ]
-	then
-		cp $FILE "$DIRECTORY"
-		echo "$FILE in ./$DIRECTORY gespeichert"
-	fi
+    ./frage.sh "Soll $FILE gesichert werden?" "y" "n"
+    ANTWORT=$?
+    if [ $ANTWORT = 0  ]
+    then
+        cp $FILE "$DIRECTORY"
+        echo "$FILE in ./$DIRECTORY gespeichert"
+    fi
 done
 
-echo "Alles klar, Ihre Dtaen wurden gesichert."
+echo "Alles klar, Ihre Daten wurden gesichert."
 
